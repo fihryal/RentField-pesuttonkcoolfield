f8fd9a850361d0b14c634006505238c405a5d259
fihryalfaqiy <fihryalfaqiy@gmail.com>
2024-05-02 09:40:52 +0700

    change color

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,11 +9,11 @@
 
             @if (Auth::check())
             <a type="button" href="/sesi/logout"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center "> Log out
+                class="text-white bg-[#00A671] hover:bg-[#127a59] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center "> Log out
             </a>
             @else
             <a type="button" href="/sesi"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Log in
+                class="text-white bg-[#00A671] hover:bg-[#127a59] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Log in
             </a>
             @endif
 
@@ -32,7 +32,7 @@
             <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
                 <li>
                     <a href="{{url('/')}}"
-                        class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:"
+                        class="block py-2 px-3 text-white bg-[#00A671] rounded md:bg-transparent md:text-[#00A671] md:p-0 md:"
                         aria-current="page">Home</a>
                 </li>
                 <li>
