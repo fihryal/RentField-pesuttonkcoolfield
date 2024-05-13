433c5c71b1bb51eba055df8d2c3dc6fe17240b40
fihryalfaqiy <fihryalfaqiy@gmail.com>
2024-05-13 08:01:02 +0700

    navbar background

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -35,9 +35,9 @@
             <ul
                 class="navbar-custom-nav flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
                 <li>
-                    <a href="{{url('/')}}"
+                    <a href="{{url('/home')}}"
 
-                        class="block py-2 px-3 text-white bg-[#00A671] rounded md:bg-transparent md:text-[#00A671] md:p-0 md:"
+                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#00A671] md:p-0 md:"
                         aria-current="page">Home</a>
 
                 </li>
